140a0eb7c299575780fa4ed93b1fb2900e99efe9
micka <146068551+mickael-ouaknin@users.noreply.github.com>
2024-02-05 01:28:02 +0100

    Add files via upload

---
+++ JOUR 1/JOB3/JOB3.sql
@@ -0,0 +1,7 @@
+CREATE TABLE etudiant (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255),
+    prenom VARCHAR(255),
+    age INT,
+    email VARCHAR(255)
+);
---
+++ JOUR 1/JOB5/JOB5.sql
@@ -0,0 +1,14 @@
+INSERT INTO etudiant (nom, prenom, age, email)
+VALUES ('Betty Spaghetti', 'Betty', 23, 'betty.Spaghetti@laplateforme.io');
+
+INSERT INTO etudiant (nom, prenom, age, email)
+VALUES ('Chuck Steak', 'Chuck', 45, 'chuck.steak@laplateforme.io');
+
+INSERT INTO etudiant (nom, prenom, age, email)
+VALUES ('John Doe', 'John', 18, 'john.doe@laplateforme.io');
+
+INSERT INTO etudiant (nom, prenom, age, email)
+VALUES ('Binkie Barnes', 'Binkie', 16, 'binkie.barnes@laplateforme.io');
+
+INSERT INTO etudiant (nom, prenom, age, email)
+VALUES ('Gertrude Dupuis', 'Gertrude', 20, 'gertrude.dupuis@laplateforme.io');
